c1d057b140f7ae91f8acb8e1701694b8d923220c
kale <intharaksak@gmail.com>
2019-08-21 22:15:52 -0400

    added database

--- tempo/server.js
+++ tempo/server.js
@@ -1,23 +1,16 @@
-const express = require("express");
-
-const mongoose = require("mongoose");
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-// app.use(routes);
+app.use(cors());
+app.use(bodyParser.json());
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
 
-// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log("Server is running on Port: " + PORT);
 });
